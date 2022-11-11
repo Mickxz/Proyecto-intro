9fa218527d5a4b301d3b8232118e2cdbe3c50444
Luxar3-cmd <ignacio.futrono@gmail.com>
2022-11-11 17:43:31 -0300

    a punto de implementar movimiento

--- Juego/views.py
+++ Juego/views.py
@@ -1,15 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 
-def index(request):
-    return render(request,"index.html")
-def about(request):
-    return render(request,"about.html")
-def base(request):
-    return render(request,"base.html")
 def mapa(request): #Para el mapa
     return render(request,"mapa.html")
 # Create your views here.
 
-
+def index(request):
+    template = "main/index.html"
+    return render (request, template)
 #tener como fondo el mapa, canva html, movement xy, plano cartesiano, js.
