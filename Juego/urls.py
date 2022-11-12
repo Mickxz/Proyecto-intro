d167a1c9fa596e798d2a6a793fc5eaa73fd36091
Davidpzo <116919292+Davidpzo@users.noreply.github.com>
2022-11-11 21:02:14 -0300

    Update urls.py

--- Juego/urls.py
+++ Juego/urls.py
@@ -2,4 +2,5 @@ from django.urls import path #Para poder usar path()
 from .import views # Estoy importando las views de la misma aplicación
 urlpatterns = [
     path('mapa/',views.mapa),
+    path('menu/',views.menu), 
 ]
