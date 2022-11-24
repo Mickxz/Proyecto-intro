66c19f354b074828e7e4fb59790e1ce8e3980e7d
Luxar3-cmd <ignacio.futrono@gmail.com>
2022-11-24 15:26:41 -0300

    asdf

--- Juego/static/CSS_JS/Overworld.js
+++ Juego/static/CSS_JS/Overworld.js
@@ -1,27 +1,11 @@
 class Overworld {
     constructor(config) {
         this.element = config.element;
-        this.canvas = this.element.querySeelector(".game-canvas");
-        this.ctx = this.canvas.getContext("2d")
         this.map = null;
     }
-    bindActionInput() {
-        KeyPressListener("Enter", () => {
-            //Hay algún personaje al cual hablarle?
-            this.map,checkForActoinCutscene()
-        })
-    }
-startMap (mapConfig) {
-    this.map = new OverworldMap(mapConfig);
-    this.map.overworld = this;
-
-}
-
 init() {
-    this.bindActionInput();
-    this.startGameLoop();
     this.map.startCutscene([
-        {type: "TextMessage",text: "This is my very first message!"}
+        {type: "TextMessage",text: "This is my very first message!"},
         ])
     }
 }
--- Juego/static/CSS_JS/OverworldEvent.js
+++ Juego/static/CSS_JS/OverworldEvent.js
@@ -4,11 +4,6 @@ class OverworldEvent {
         this.event = event;
     }
     textMessage(resolve) { //Funcion de mensaje, como evento.
-
-        if (this.event.faceHero) { 
-            const obj = this.map.gameObjects[this.event.faceHero];
-            obj.direction = utils.oppositeDirection ( this.map.gameObjects["hero"].direction);
-        }
         const message = new TextMessage({
             text: this.event.text,
             onComplete: () => resolve()
@@ -16,16 +11,6 @@ class OverworldEvent {
         message.init( document.querySelector(".game-container") )
     }
 
-    changeMap(resolve) {
-        const sceneTransition = new SceneTransition();
-        sceneTransition.init(document,querySelector(".game-container"), () => {
-            this.map.overworld.startMap ( window.OverworldMaps[this.event.map] );
-            resolve();
-
-            sceneTransition.fadeOut();
-
-        })
-    }
     init() {
         return new Promise(resolve => {
             this[this.event.type](resolve)
